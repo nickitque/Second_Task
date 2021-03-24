00de2c7bc4980d082368032984d9f5f04f5a5cb5
nickitque <58918756+nickitque@users.noreply.github.com>
2021-03-24 22:49:14 +0300

    Adding assert messages

--- tests/test_2_shotgun.py
+++ tests/test_2_shotgun.py
@@ -2,5 +2,5 @@
 def shotgun(find_shotgun_phrase):
 
     phrase = "По запросу «shotgun» ничего не найдено"
-    assert find_shotgun_phrase.status_code == 200
-    assert phrase in find_shotgun_phrase.text
+    assert find_shotgun_phrase.status_code == 200, "The status code is 200, connection is ok"
+    assert phrase in find_shotgun_phrase.text, "OK. Shotgun vacancies are not found"
