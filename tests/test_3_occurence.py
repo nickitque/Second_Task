9fe60a2a94c90e34cc0b1bfce729914f1f8e4d51
nickitque <58918756+nickitque@users.noreply.github.com>
2021-03-24 22:41:06 +0300

    Changing methods name

--- tests/test_3_occurence.py
+++ tests/test_3_occurence.py
@@ -1,8 +1,8 @@
 
-def occurence(grab_total_vacancies_list,search_number_of_vacancies):
+def occurence(grab_total_vacancies_list, get_vacancies):
 
     actual_number_of_links = len(grab_total_vacancies_list)
-    expected_number_of_links = search_number_of_vacancies
+    expected_number_of_links = get_vacancies
     assert expected_number_of_links == actual_number_of_links
 
 
