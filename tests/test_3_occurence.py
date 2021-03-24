88107431164b8e4f7538c1d2c8139e5d715174bd
nickitque <58918756+nickitque@users.noreply.github.com>
2021-03-24 22:50:08 +0300

    Adding assert message

--- tests/test_3_occurence.py
+++ tests/test_3_occurence.py
@@ -3,6 +3,6 @@ def occurence(grab_total_vacancies_list, get_vacancies):
 
     actual_number_of_links = len(grab_total_vacancies_list)
     expected_number_of_links = get_vacancies
-    assert expected_number_of_links == actual_number_of_links
+    assert expected_number_of_links == actual_number_of_links, "OK. Expected number is equal to actual number"
 
 
