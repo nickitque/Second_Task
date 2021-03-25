205e611e02ba73d579555377d4a622e7fca416a3
nickitque <58918756+nickitque@users.noreply.github.com>
2021-03-25 13:18:18 +0300

    Adding assert and assert message

--- tests/test_1_connection.py
+++ tests/test_1_connection.py
@@ -1,5 +1,8 @@
 
 def connection_pytest(connection_test):
+    """Getting status code"""
     status_code =  connection_test.status
     status_info = connection_test.reason
-    assert status_code == 200 and status_info == 'OK'
+    assert status_code == 200, "connection established"
+    assert status_info == 'OK'
+    
