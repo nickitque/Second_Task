5bfca97c2c82a228fab9d5f758bc29a52eb1fdbb
nickitque <58918756+nickitque@users.noreply.github.com>
2021-03-25 15:22:54 +0300

    Assert messages changes

--- tests/test_1_connection.py
+++ tests/test_1_connection.py
@@ -3,6 +3,6 @@ def connection_pytest(connection_test):
     """Getting status code"""
     status_code =  connection_test.status
     status_info = connection_test.reason
-    assert status_code == 200, "connection established"
-    assert status_info == 'OK'
+    assert status_code == 200, "Status code is 200"
+    assert status_info == 'OK', "Connection test passed"
     
